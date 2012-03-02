8f318c366f41da4d791ba910c09f21e4f9b969ee
Calvin Szeto <szeto.calvin@gmail.com>
2012-03-02 03:50:30 -0600

    Add init, extend_heap, and coalesce functions based on B&H book implementation.

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -23,16 +23,16 @@
  * provide your team information in the following struct.
  ********************************************************/
 team_t team = {
-    /* Team name */
-    "ateam",
-    /* First member's full name */
-    "Harry Bovik",
-    /* First member's email address */
-    "bovik@cs.cmu.edu",
-    /* Second member's full name (leave blank if none) */
-    "",
-    /* Second member's email address (leave blank if none) */
-    ""
+	/* Team name */
+	"ateam",
+	/* First member's full name */
+	"Calvin Szeto",
+	/* First member's email address */
+	"szeto.calvin@gmail.com",
+	/* Second member's full name (leave blank if none) */
+	"Matthew Granado",
+	/* Second member's email address (leave blank if none) */
+	"mattg@mail.utexas.edu"
 };
 
 /* single word (4) or double word (8) alignment */
@@ -44,12 +44,96 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+#define WSIZE 4 //word size
+#define DSIZE 8 //double word size
+#define CHUNKSIZE (1<<12) //extend the heap 2**10 words at a time
+
+#define MAX(x,y) ((x) > (y)? (x) : (y)) //max of x and y
+
+//size is a multiple of 8 so last three bits are available for alloc status
+#define PACK(size,alloc) ((size)|(alloc))
+
+//dereferences p, must cast first since p is type void *
+#define GET(p) (*(unsigned int *)(p))
+#define PUT(p,val) (*(unsigned int *)(p)=(val))
+
+//gets size or alloc status from a pointer
+#define GET_SIZE(p) (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+//given a block pointer, returns header or footer, could change if footer size changes
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+//given block pointer, gives next or previous block pointer
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+
+static char *heap_listp;
+
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
-    return 0;
+	if((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1)
+		return -1;
+	PUT(heap_listp,0);
+	PUT(heap_listp+(1*WSIZE),PACK(DSIZE,1));
+	PUT(heap_listp+(2*WSIZE),PACK(DSIZE,1));
+	PUT(heap_listp+(3*WSIZE),PACK(0,1));
+
+	if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
+		return -1;
+	return 0;
+}
+
+static void *extend_heap(size_t words)
+{
+	char *bp;
+	size_t size;
+	size=(words % 2) ? (words+1) * WSIZE : words * WSIZE;
+	if((long)(bp=mem_sbrk(size)) == -1)
+		return NULL;
+
+	PUT(HDRP(bp),PACK(size,0));
+	PUT(FTRP(bp),PACK(size,0));
+	PUT(HDRP(NEXT_BLKP(bp)),PACK(0,1));
+
+	return coalesce(bp);
+}
+
+static void *coalesce(void *bp)
+{
+	size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+	size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+	size_t size = GET_SIZE(HDRP(bp));
+
+	if (prev_alloc && next_alloc) {
+		return bp;
+	}
+
+	else if (prev_alloc && !next_alloc) {
+		size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+		PUT(HDRP(bp), PACK(size,0));
+		PUT(FTRP(bp), PACK(size,0));
+	}
+
+	else if (!prev_alloc && next_alloc) {
+		size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+		PUT(FTRP(bp), PACK(size,0));
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size,0));
+		bp=PREV_BLKP(bp);
+	}
+
+	else {
+		size += GET_SIZE(HDRP(PREV_BLKP(bp)))+
+			GET_SIZE(HDRP(NEXT_BLKP(bp)));
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size,0));
+		PUT(FTRP(NEXT_BLKP(bp)), PACK(size,0));
+		bp=PREV_BLKP(bp);
+	}
+	return bp;
 }
 
 /* 
@@ -58,14 +142,14 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-	return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
-    }
+	int newsize = ALIGN(size + SIZE_T_SIZE);
+	void *p = mem_sbrk(newsize);
+	if (p == (void *)-1)
+		return NULL;
+	else {
+		*(size_t *)p = size;
+		return (void *)((char *)p + SIZE_T_SIZE);
+	}
 }
 
 /*
@@ -80,19 +164,19 @@ void mm_free(void *ptr)
  */
 void *mm_realloc(void *ptr, size_t size)
 {
-    void *oldptr = ptr;
-    void *newptr;
-    size_t copySize;
-    
-    newptr = mm_malloc(size);
-    if (newptr == NULL)
-      return NULL;
-    copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
-    if (size < copySize)
-      copySize = size;
-    memcpy(newptr, oldptr, copySize);
-    mm_free(oldptr);
-    return newptr;
+	void *oldptr = ptr;
+	void *newptr;
+	size_t copySize;
+
+	newptr = mm_malloc(size);
+	if (newptr == NULL)
+		return NULL;
+	copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
+	if (size < copySize)
+		copySize = size;
+	memcpy(newptr, oldptr, copySize);
+	mm_free(oldptr);
+	return newptr;
 }
 
 
